c3afd8c22cb05a26d9e612a5d21e15596dea3c84
GangsterMy <impzone@163.com>
2016-08-27 22:51:45 +0800

    first login

--- Practice/ViewController.m
+++ Practice/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBar.barTintColor = appGreen;
+    [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], UITextAttributeTextColor, nil]];
+    self.navigationItem.title = @"应用名称";
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
